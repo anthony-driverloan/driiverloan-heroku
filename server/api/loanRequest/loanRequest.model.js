df09d6128f8da4cebaae7b964eb86a8f60212c54
Anthony Akinmoyewa <Anthony@Anthonys-MacBook-Air.local>
2015-06-11 03:49:02 +0100

    Built driverloan-v1 from commit 9cd106a on branch master

--- server/api/loanRequest/loanRequest.model.js
+++ server/api/loanRequest/loanRequest.model.js
@@ -4,13 +4,13 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var LoanRequestSchema = new Schema({
-  // loanAmount: {type: Number, required: true},
-  // loanTerm: {type: Number, required: true},
-  // monthlyPayments: {type:Number, required: true},
-  // totalPayment: {type:Number, required:true},
-  // decision: {type:String, required:true, default: 'processing'},
-  // userId: {type:Schema.Types.ObjectId, required:true},
-  data: String
+  loanAmount: {type: Number, required: true},
+  loanTerm: {type: String, required: true},
+  loanPurpose: {type: String, required: true},
+  monthlyPayments: {type:Number, required: true, default:163.34},
+  totalPayment: {type:Number, required:true, default:2000},
+  decision: {type:String, required:true, default: 'processing'},
+  // userId: {type:Schema.Types.ObjectId, required:true}
 });
 
 module.exports = mongoose.model('LoanRequest', LoanRequestSchema);
