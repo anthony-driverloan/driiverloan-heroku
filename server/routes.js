81b8709b15745b72295f92077c708dd13bfa7d27
Anthony Akinmoyewa <Anthony@Anthonys-MacBook-Air.local>
2015-06-12 01:03:29 +0100

    Built driverloan-v1 from commit 3dc5dd1 on branch master

--- server/routes.js
+++ server/routes.js
@@ -9,6 +9,7 @@ var errors = require('./components/errors');
 module.exports = function(app) {
 
   // Insert routes below
+  app.use('/api/urls', require('./api/url'));
   app.use('/api/loanRequests', require('./api/loanRequest'));
   app.use('/api/vehicles', require('./api/vehicle'));
   app.use('/api/postcodes', require('./api/postcode'));
